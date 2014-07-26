b313c19b4e3c1f6182e1be19b698e5b85dc15b21
Kevin McDermott <kevin.t.mcdermott@gmail.com>
2014-07-25 22:52:54 -0600

    First, untested, super simple iteration of mu_man

--- src/mu_man/mu_man.py
+++ src/mu_man/mu_man.py
@@ -16,7 +16,7 @@
 # 5 = mu-man starting pos
 # 6 = ghost staring pos
 #
-#The Lambda-Man status is a 5-tuple consisting of:
+# The Lambda-Man status is a 5-tuple consisting of:
 #  1. Lambda-Man's vitality;
 #  2. Lambda-Man's current location, as an (x,y) pair;
 #  3. Lambda-Man's current direction;
@@ -59,13 +59,71 @@ class MuMan:
         mu_man_vit, mu_man_pos, mu_man_dir, mu_man_lives, mu_man_score = mu_man_state
 
         # we have 4 possible moves, so see which are valid (aren't walls) and evaluate each one
-        #
-        
-        
+        # case: up
+        x, y = mu_man_pos
+        up_y = y -1
 
-    def calc_hn(state, move, self)
-        
+        closest_dist = 2,147,483,647 # max signed int
+
+        # check if it's a wall
+        if mu_world[up_y[x]] != 0:
+            closest_dist = calc_hn(state, 0)
+            move = 0
+
+        # case: down
+        down_y = y +1
+        # check if it's a wall
+        if mu_world[down_y[x]] != 0:
+            new_dist = calc_hn(state, 2)
+            if closest_dist > new_dist:
+                closest_dist = new_dist
+                move = 2
+
+        # case: right
+        right_x = x +1
+        # check if it's a wall
+        if mu_world[y[right_x]] != 0:
+            calc_hn(state, 1)
+            if closest_dist > new_dist:
+                closest_dist = new_dist
+                move = 1
+
+        # case: right
+        left_x = x -1
+        # check if it's a wall
+        if mu_world[y[left_x]] != 0:
+            calc_hn(state, 3)
+            if closest_dist > new_dist:
+                closest_dist = new_dist
+                move = 3
+
+        return (state, move)
 
 
-if __name__ == "__main__":
+    def calc_hn(state, move, self):
+        # current heuristic: move towards closest pill
+        closest_manh = 2,147,483,647 # max signed int
+        closest_x = 0
+        closest_y = 0
 
+        for y in range(0, len(mu_world)):
+            for x in range(0, len(mu_world[y]):
+                if mu_world[y[x]] in [2,3,4]:
+                    # we have something we want to move towards
+                    manh_dist = calc_manh_distance(x, y, state)
+                    
+                    if closest_manh > manh_dist:
+                        closest_manh = manh_dist
+                        closest_x = x
+                        closest_y = y
+
+        return (closest_x, closest_y, closest_manh)
+
+
+    def calc_manh_distance(x, y, state):
+        mu_world, mu_man_state, ghosts, fruit = state
+        mu_man_vit, mu_man_pos, mu_man_dir, mu_man_lives, mu_man_score = mu_man_state
+
+        mu_x, mu_y = mu_man_pos
+
+        return abs(x - mu_x) + abs(y - mu_y)
